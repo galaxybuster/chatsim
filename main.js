c5a1a3677256c71b296fef67b212b7414d6dca68
U-SUNYSB.EDU\racchen <racchen@CS-HELPDESK-03.campus.stonybrook.edu>
2016-02-24 15:41:53 -0500

    Passport is now a dependency

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(__dirname+'/frontend'));
+app.use(express.static(__dirname + '/frontend'));
+app.use(function(req, res, next) {
+    console.log('%s %s', req.method, req.url);
+    next();
+});
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/frontend/index.html'));
