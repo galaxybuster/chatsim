d95069472a955930f360a3b14ff1c7f08d2c64d5
U-SUNYSB.EDU\racchen <racchen@CS-HELPDESK-03.campus.stonybrook.edu>
2016-02-24 14:18:38 -0500

    Server now serves js files correctly. Moved frontend site details to the frontend folder

--- main.js
+++ main.js
@@ -2,12 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use('/css',express.static(__dirname+'/css'));
-app.use('/img',express.static(__dirname+'/img'));
-app.use('/js',express.static(__dirname+'/js'));
+app.use(express.static(__dirname+'/frontend'));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/frontend/index.html'));
 });
 
 app.listen(3000, function () {
